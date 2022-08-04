7ee2e2d0a0e7cb0013c60af53e08257c3ebba630
Montana Mendy <montana@linux.com>
2022-08-03 19:40:07 -0700

    Add state, token state in particular.

---
+++ ProtectedRoutes.jsx
@@ -0,0 +1,24 @@
+// Imports omitted
+
+export const ProtectedRoutes = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+    
+  const { accessToken } = useSelector(state => state.auth); // Fetch access token from state
+
+  const { data, isError } = useRefreshAccessTokenQuery(); 
+
+  if (isError) {
+    navigate('/login');
+  }
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setCredentials(data));
+    }
+  }, [data, dispatch]);
+
+  if (accessToken) return <Outlet />; 
+
+  return <h1>Loading...</h1>;
+};

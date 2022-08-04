9c977c4b5ca3256ee7721c5a42819a336abcd380
Montana Mendy <montana@linux.com>
2022-08-03 19:41:14 -0700

    Possibly use promises in new version.

---
+++ register.controller.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model.js';
+
+export const register = async (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  const newRefreshToken = jwt.sign({ email }, REFRESH_TOKEN_SECRET, '36h');
+
+  try {
+    const createdUser = await User.create({
+      ...req.body,
+      password: await bcrypt.hash(password, 10),
+      refreshToken: newRefreshToken,
+    });
+
+    const { _id: id, ...rest } = createdUser._doc;
+
+    res.cookie('refresh_token', newRefreshToken, {
+      httpOnly: true,
+    });
+
+    const newAccessToken = jwt.sign({ email, id }, ACCESS_TOKEN_SECRET, '2h');
+
+    res.status(200).json({
+      user: { id, accessToken: newAccessToken, ...rest },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
